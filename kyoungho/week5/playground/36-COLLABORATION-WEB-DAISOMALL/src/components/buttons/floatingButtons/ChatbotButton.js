075702f59816e2d9db463eec8506c7e1769ee991
Kyoungho Eom <kheom0512@gmail.com>
2025-06-19 22:54:40 +0900

    feat: js 파일 제거

--- kyoungho/week5/playground/36-COLLABORATION-WEB-DAISOMALL/src/components/buttons/floatingButtons/ChatbotButton.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "@emotion/react/jsx-runtime";
-// SVG 컴포넌트를 실제 SVG 파일로 변경
-import * as S from './ChatbotButton.style';
-const ChatbotButton = () => (_jsx("button", { css: S.buttonStyle, "aria-label": "\uCC57\uBD07 \uC0C1\uB2F4", children: _jsx("img", { src: "/svgs/chatbot-icon.svg", alt: "\uCC57\uBD07", css: S.iconStyle }) }));
-export default ChatbotButton;
